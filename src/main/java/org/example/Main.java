86eee0002560fea1b5a78237de2633bc66ebea29
yak.anutik <yak.anutik@gmail.com>
2023-06-05 18:43:33 +0300

    [TASK-002] created variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,4 +2,16 @@ package org.example;
 
 public class Main {
 
+    public static void main(String[] args) {
+
+        // create variables
+        byte w;
+        short g;
+        int j;
+        long f;
+        char y;
+        double d;
+        float t;
+        boolean z;
+    }
 }
