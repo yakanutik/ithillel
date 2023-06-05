b83f9d0bcb4a88464fb48e669842a49a8eb267a4
yak.anutik <yak.anutik@gmail.com>
2023-06-05 18:45:38 +0300

    [TASK-002] init variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,5 +13,15 @@ public class Main {
         double d;
         float t;
         boolean z;
+
+        // init variables
+        w = 5;
+        g = 135;
+        j = 15;
+        f = 1345;
+        y = 't';
+        d = 1.25;
+        t = 1.45f;
+        z = true;
     }
 }
