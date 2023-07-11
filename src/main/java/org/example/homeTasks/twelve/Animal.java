c6abf3c3ac476cb9218cbd4586221f7eb07438b9
yak.anutik <yak.anutik@gmail.com>
2023-07-11 20:56:58 +0300

    TASK-012: add first task

---
+++ src/main/java/org/example/homeTasks/twelve/Animal.java
@@ -0,0 +1,9 @@
+package org.example.homeTasks.twelve;
+
+public class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/org/example/homeTasks/twelve/Dog.java
@@ -0,0 +1,10 @@
+package org.example.homeTasks.twelve;
+
+public class Dog extends Animal {
+    String breed;
+
+    public Dog(String name, String breed) {
+        super(name);
+        this.breed = breed;
+    }
+}
